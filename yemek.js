dde375c28b8a982cbb9d17e246d7bae76fa21b48
Kaan Cinar <43555453+aydinkaancinar@users.noreply.github.com>
2020-09-15 15:28:33 -0400

    Update yemek.js

--- yemek.js
+++ yemek.js
@@ -4,8 +4,8 @@ function Yemek(){
   var counter = 0;
   blockSize2 = 7;
   this.pickLocation = function(){
-    var col = (Math.floor(Math.random() * 500) + 100);
-    var row = (Math.floor(Math.random() * 500) + 100);
+    var col = (Math.floor(Math.random() * 400) + 100);
+    var row = (Math.floor(Math.random() * 400) + 100);
     this.x = col - col%20;
     this.y = row - row%20;
     counter++;
